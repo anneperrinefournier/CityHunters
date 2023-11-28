50c4ac56c858dd4039fe87a55a5dca858917d0ec
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2023-11-28 15:59:23 +0100

    configuration of routes games, controller game and views lobby, create and show

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ApplicationRecord
   belongs_to :user
   belongs_to :storyline
+
+  enum status: {
+    not_started: 0,
+    started: 1,
+    finished: 2
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_105752) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_111504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_105752) do
     t.bigint "storyline_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pin"
     t.index ["storyline_id"], name: "index_games_on_storyline_id"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
