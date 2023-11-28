6b63f145a124fad587eb9699c853047a7734cb4d
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-11-28 16:50:08 +0100

    map

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+# map geocoder
+gem "geocoder"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
   belongs_to :storyline
   has_many :riddles, dependent: :destroy
   validates :name, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
