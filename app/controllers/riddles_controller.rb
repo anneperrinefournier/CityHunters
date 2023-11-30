cc9349548daf44b283b524846e55195e25dea06b
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2023-11-30 22:23:45 +0100

    Intermediary save user answer

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -1,12 +1,18 @@
 class RiddlesController < ApplicationController
   def verify
     riddle_id = params[:question][:riddle_id].to_i
-    user_answer = params.dig(:question, :answer)
+    game = Game.find(params[:question][:game_id].to_i)
 
-    # params # DATA form the form
-
+    user_answer = Answer.new(
+      game: game,
+      user: current_user,
+      riddle_id: riddle_id,
+      content: params.dig(:question, :answer)
+    )
     riddle = Riddle.find(riddle_id)
-    riddle_answer = riddle.content
+
+    debugger
+
 
     place = Place.find(riddle.place_id)
     riddles = Riddle.where(place: place)
@@ -18,7 +24,8 @@ class RiddlesController < ApplicationController
     end
 
     # check if the answer is correct
-    if user_answer == riddle_answer
+    if user_answer.content == riddle.solution
+      user_answer.correct = true
 
       GameChannel.broadcast_to(
         "game-#{@game.id}",
@@ -26,7 +33,7 @@ class RiddlesController < ApplicationController
       )
       render json: {
         status: :ok,
-        next_riddle: render_to_string(partial: 'next_riddle', formats: [:html], locals: { riddle: next_riddle })
+        next_riddle: render_to_string(partial: 'game_state', formats: [:html], locals: { riddle: next_riddle })
       }
 
       # render json: {
@@ -40,26 +47,4 @@ class RiddlesController < ApplicationController
       }
     end
   end
-
-    # debugger
-
-    # if next_riddle_index < riddles.size
-    #   next_riddle = Riddle.all[next_riddle_index]
-    # else
-    #   puts "prout"
-    # end
-
-
-
-    # if correct
-     # respond a json object with the html of the next riddle
-    # else
-      # respond a json object with error message
-     # end
-
-    # if user_answer == riddle.content
-    #   render json: { status: :ok, next_riddle_html: render_to_string(partial: 'next_riddle', locals: { riddle: next_riddle }) }
-    # else
-    #   render json: { status: :error, message: "Incorrect answer. Try again." }
-    # end
 end
