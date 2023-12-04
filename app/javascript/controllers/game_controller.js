e51605336cc6c6b8fb21db2a242d9e93e87f1086
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2023-12-04 12:30:42 +0100

    Bugfix geomap

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -6,18 +6,33 @@ export default class extends Controller {
   static values = {
     id: Number,
     apiKey: String,
-    markers: Array
+    markers: Array,
+    participationsMarkers: Array
   }
-  static targets = ['pageHandle', 'riddlesHandle', 'introduction', 'enigme', 'placeTabs', 'placeTab', 'placePanel', 'displayAnswerBtn', 'map', 'endGameButton', 'endGame'];
 
-  connect() {
-    console.log('Game controller connected')
+  static targets = [
+    'pageHandle',
+    'riddlesHandle',
+    'introduction',
+    'enigme',
+    'placeTabs',
+    'placeTab',
+    'placePanel',
+    'displayAnswerBtn',
+    'map',
+    'endGameButton',
+    'endGame'
+  ];
+
+  initialize() {
     this.token = document.querySelector('meta[name="csrf-token"]').content
     this.channel = createConsumer().subscriptions.create(
       { channel: "GameChannel", id: this.idValue },
       { received: data => this.#handleData(data) }
     )
+  }
 
+  connect() {
     navigator.geolocation.watchPosition((coordinates) => {
       this.channel.send({
         participation_id: 1, // participation_id,
@@ -26,20 +41,39 @@ export default class extends Controller {
       })
     })
 
+    this._initMap();
+    console.log(this.participationsMarkersValue);
+  }
+
+  _initMap() {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
+      container: this.mapTarget,
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
     this.#addMarkersToMap()
+    this.#addPlayerMakersToMap()
     this.#fitMapToMarkers()
   }
 
-  handleData(data) {
+  #handleData(data) {
     if (data.type === 'redirect') {
       window.location.href = data.url;
+    }
+
+    if (data.type === "update_position") {
+      const marker = this.playerMarkers.filter(item => item.participation_id === data.participation_id)
+      marker.setLngLat([ data.lng, data.lat ])
+    }
+
+    if (data.game_status == 'ended') {
+      this.pageHandleTarget.innerHTML = data.content;
+    } else if (data.game_status == 'running') {
+      this.riddlesHandleTarget.innerHTML = data.content;
+      this.displayAnswerBtnTarget.classList.remove('d-none');
+      this.displayAnswerBtnTarget.scrollIntoView(true)
     }
   }
 
@@ -74,20 +108,6 @@ export default class extends Controller {
     }).classList.remove('d-none')
   }
 
-  #handleData(data) {
-    if (data.type === 'redirect') {
-      window.location.href = data.url;
-    }
-
-    if (data.game_status == 'ended') {
-      this.pageHandleTarget.innerHTML = data.content;
-    } else if (data.game_status == 'running') {
-      this.riddlesHandleTarget.innerHTML = data.content;
-      this.displayAnswerBtnTarget.classList.remove('d-none');
-      this.displayAnswerBtnTarget.scrollIntoView(true)
-    }
-  }
-
   async endGame() {
     const options = {
       method: 'GET',
@@ -100,20 +120,31 @@ export default class extends Controller {
     const response = await fetch(`/games/${this.idValue}/end`, options);
   }
 
+  #addPlayerMakersToMap() {
+    this.playerMarkers = []
+
+    this.participationsMarkersValue.forEach((marker) => {
+      this.playerMarkers.push({
+        participation_id: marker.participation_id,
+        marker: new mapboxgl.Marker()
+                            .setLngLat([ marker.lng, marker.lat ])
+                            .addTo(this.map)
+      })
+    })
+  }
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
 
       const customMarker = document.createElement('div');
       customMarker.className = `marker ${marker.marker_class}`;
-
-
       customMarker.innerHTML = marker.marker_html
 
       new mapboxgl.Marker(customMarker)
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(this.map)
+                  .setLngLat([ marker.lng, marker.lat ])
+                  .setPopup(popup)
+                  .addTo(this.map)
     })
   }
 
