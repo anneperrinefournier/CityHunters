2c7a2eecda11c985119f0e9da054069dfeb5b498
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-12-04 10:09:12 +0100

    css + seed

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -3,4 +3,19 @@ class GameChannel < ApplicationCable::Channel
     game = Game.find(params[:id])
     stream_for "game-#{game.id}"
   end
+
+  def receive(data)
+    # participation = Participation.find(data['participation_id'])
+    # participation.update(latitude: data['latitude'], longitude: data['longitude'])
+    # game = participation.game
+
+    # participation.reload
+
+    # GameChannel.broadcast_to("game-#{game.id}", {
+    #   action: 'update_participation',
+    #   participation_id: participation.id,
+    #   longitude: participation.longitude,
+    #   latitude: participation.latitude
+    # })
+  end
 end
--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,16 +1,51 @@
 import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
+import mapboxgl from 'mapbox-gl' // Don't forget this!
 
 // Connects to data-controller="game"
+// Connects to data-controller="map"
+
 export default class extends Controller {
-  static values = { id: Number }
+  static values = {
+    id: Number,
+    apiKey: String,
+    markers: Array
+  }
   static targets = ['riddle', 'introduction', 'enigme', 'placeTabs', 'placeTab', 'placePanel', 'displayAnswerBtn'];
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "GameChannel", id: this.idValue },
-      { received: data => console.log(data) }
+      { received: data => #handleData(data) }
     )
+
+    navigator.geolocation.watchPosition((coordinates) => {
+      this.channel.send({
+        participation_id: // participation_id,
+        longitude: coordinates.coords.longitude,
+        latitude: coordinates.coords.latitude,
+      })
+    })
+
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #handleData(data) {
+    // if (data.action === 'update_participation') {
+    //   this.playerMarkers
+    //       .find(item => item.id == data.participation_id)
+    //       .marker.setLngLat([data.longitude, data.latitude])
+
+    //   return;
+    // }
   }
 
   closeIntroduction() {
@@ -21,10 +56,11 @@ export default class extends Controller {
 
   activate(event) {
     event.preventDefault()
-    console.log(this.placeTabTargets)
+
     this.placeTabTargets.forEach(tab => {
       tab.classList.remove('active')
     });
+
     event.currentTarget.classList.add('active');
   }
 
@@ -38,5 +74,28 @@ export default class extends Controller {
     this.placePanelTargets.find( (panel) => {
       return panel.dataset.index == tabIndex
     }).classList.remove('d-none')
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+      const customMarker = document.createElement('div');
+      customMarker.className = `marker ${marker.marker_class}`;
+
+
+      customMarker.innerHTML = marker.marker_html
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
