a7c1efc0e3debc1a893eb4ddae8e88a22c7733a0
pierrefrancois thomas <pf-thomas@hotmail.fr>
2023-12-04 18:35:52 +0100

    swipe almost final

--- app/javascript/controllers/swipe_map_controller.js
+++ app/javascript/controllers/swipe_map_controller.js
@@ -2,14 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="swipe-map"
 export default class extends Controller {
-  static targets = ["swipe", "unroll"]
+  static targets = ["swipe", "unroll", "police"]
   connect() {
     console.log("hello from the swipe controller")
   }
 
   swipe() {
     console.log("ça marche?")
-    this.unrollTarget.style.transform = "translateX(-100%)";
-    this.swipeTarget.style.transform = "translateX(100%)";
+    if (this.unrollTarget.style.transform === "translateX(-100vw)") {
+      this.unrollTarget.style.transform = "translateX(0vw)";
+    } else {
+      this.unrollTarget.style.transform = "translateX(-100vw)";
+      this.policeTarget.style.paddingLeft = "15px";
+    }
+    // this.swipeTarget.style.transform = "translateX(100%)";
   }
 }
