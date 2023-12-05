6fe0dae9840cf9da9094cf9d1f25063c56a8acd5
pierrefrancois thomas <pf-thomas@hotmail.fr>
2023-12-05 18:05:23 +0100

    finally finalized the finalization of the final swipe map behavioral swiping finalistic swipe

--- app/javascript/controllers/swipe_map_controller.js
+++ app/javascript/controllers/swipe_map_controller.js
@@ -2,18 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="swipe-map"
 export default class extends Controller {
-  static targets = ["swipe", "unroll", "police"]
+  static targets = ["swipe", "unroll", "police", "showmap"]
   connect() {
     console.log("hello from the swipe controller")
-  }
+
+
+    const bigContainer = document.querySelector('.big-container');
+    const stickyElement = document.querySelector('.parent');
+    const rectangle = document.querySelector('.rectangle-swipe');
+
+    window.addEventListener('scroll', (event) => {
+
+      const scrolly = window.scrollY
+      console.log(scrolly)
+        stickyElement.style.top = `${scrolly}px`;
+      });
+    }
 
   swipe() {
     console.log("ça marche?")
     if (this.unrollTarget.style.transform === "translateX(-100vw)") {
       this.unrollTarget.style.transform = "translateX(0vw)";
+      this.policeTarget.style.marginLeft = "400px";
+      this.showmapTarget.classList.remove("font-police");
+      this.showmapTarget.style.marginLeft = "-14px";
+
     } else {
       this.unrollTarget.style.transform = "translateX(-100vw)";
+      this.policeTarget.style.marginLeft = "-30px";
+      this.showmapTarget.classList.add("font-police");
+      this.showmapTarget.style.marginLeft = "14px";
     }
     // this.swipeTarget.style.transform = "translateX(100%)";
-  }
+  };
 }
