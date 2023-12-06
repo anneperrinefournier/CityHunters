212082f6d6344a4dc85ea6348af3aa222bcc4067
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2023-12-06 15:25:15 +0100

    seed + errors messages

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,9 @@ Rails.application.routes.draw do
 
   post '/verify', to: 'riddles#verify'
   patch '/update', to: 'participations#update'
+
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
+  get '/422', to: 'errors#unprocessable'
+
 end
