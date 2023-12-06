685230fead4dac121445e45734defed53a398a4e
pierrefrancois thomas <pf-thomas@hotmail.fr>
2023-12-06 12:17:55 +0100

    started links on burger

--- app/javascript/controllers/burger_controller.js
+++ app/javascript/controllers/burger_controller.js
@@ -5,4 +5,16 @@ export default class extends Controller {
   connect() {
     console.log("Happy meal or maxi best of?")
   }
+
+  async endGame() {
+    const options = {
+      method: 'GET',
+      headers: {
+        "Accept": "application/json",
+        "X-CSRF-TOKEN": this.token
+      },
+    };
+
+    const response = await fetch(`/games/${this.gameIdValue}/end`, options);
+  }
 }
