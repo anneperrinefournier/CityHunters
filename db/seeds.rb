23da7a92e9ded16cc26ae929e267f906caddaae3
henri guillot <henri.guillot@hotmail.com>
2023-12-08 17:45:25 +0100

    efklsdhvlksdhv

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,6 +96,9 @@ file = URI.open("https://i.ibb.co/SXNkWx0/re-publique.jpg")
 storyline2.photo.attach(io: file, filename: "Code Rouge", content_type: "image/jpg")
 storyline2.save!
 
+
+
+
 storyline3 = Storyline.new(
   title: "L’Odyssée du Château",
   district: "19ᵉ arr.",
@@ -438,15 +441,7 @@ clue.save!
 # place1.photo.attach(io: file, filename: "lewagon", content_type: "image/jpg")
 # place1.save!
 
-# place2 = Place.new(
-#   name: "Metro Parmentier",
-#   address: "86 avenue Parmentier, 75004 Paris, France",
-#   description: "Cette bouche de metro... que dire dessus sinon que je ne l'ai jamais prise!",
-#   storyline: storyline
-# )
-# file = URI.open("https://p.monumentum.fr/galerie/maxi/00446/446476-francais-entree-de-la-station-de-metro-parmentier-avenue-parmentier-paris.jpg")
-# place2.photo.attach(io: file, filename: "metro parmentier", content_type: "image/jpg")
-# place2.save!
+#
 # place3 = Place.new(
 #   name: "Boulangerie Oberkampf",
 #   address: "54 rue Oberkampf, 75001 Paris, France",
@@ -544,6 +539,207 @@ clue.save!
 # riddle7.photo.attach(io: file, filename: "eat_together", content_type: "image/jpg")
 # riddle7.save!
 
+# storyline bonus
+
+storyline_bonus = Storyline.new(
+  title: "Ruby, c'est finiii",
+  district: "11ᵉ arr.",
+  duration: 20,
+  distance: 6,
+  theme: "grosse teuf",
+  difficulty: 3,
+  starting_point: "68 avenue Parmentier, 75011 Paris, France",
+  short_description: "Party of your life",
+  long_description: "Comment se rendre à une soirée épique de fin de batch après deux mois de code intensif.",
+  introduction: "Vous êtes épuisé. Épuisé mais ravi, encore porté par l’adrénaline de cette journée. Deux mois intenses viennent de s’écouler dans les murs du Wagon. Votre application n’a pas crashé pendant la démonstration ( enfin on espère^^), maintenant s’ouvre devant vous le scintillement d’une soirée trépidante. Pas de controller en vue, c’est le moment d’oublier tout vos bundle install et autres folles gem, queryselector et elsif, bootstrap et fetch…c’est le moment pour un rm-rf de qualité, un reboot complet, délestez-vous de vos git push origin (master??) en fredonnant 'Ruby, c'est finiiiii, et dire que c'était le code de mon premier amoooour!' "
+)
+
+# riddle1 = Riddle.new(
+#   title: "Ce que je préfère à 13h",
+#   description: 'Find the next place',
+#   question: "S'il y a bien une chose que je préfère quand l'heure de la soupe arrive, c'est...?",
+#   solution: "manger",
+#   motion_type: 'static',
+#   place: place1
+# )
+# file = URI.open('https://fondationolo.ca/wp-content/uploads/2017/07/fondation-olo-bebe-a-faim.jpg')
+# riddle1.photo.attach(io: file, filename: "eat_together", content_type: "image/jpg")
+# riddle1.save!
+
+
+file = URI.open("https://media.sudouest.fr/16036980/1000x500/20230723111519-copiedebi7963.jpg?v=1690178400")
+storyline_bonus.photo.attach(io: file, filename: "teuf", content_type: "image/jpg")
+storyline_bonus.save!
+
+# 101
+place101 = Place.new(
+  name: "101",
+  address: "68 Avenue Parmentier, 75011 Paris",
+  description: "Alors en route, get/batch_party_path(bière), alcools.querySelectorAll(« babyguinness »), soirée.addEventListener('danse' -> Soirée.new(batch_party)",
+  storyline: storyline_bonus
+)
+file = URI.open("https://i.ibb.co/Kmjtv1T/le-wagon.jpg")
+place101.photo.attach(io: file, filename: "LeWagon", content_type: "image/jpg")
+place101.save!
+
+# 102
+place102 = Place.new(
+  name: "Lycée Charles Péguy",
+  address: "80 avenue Parmentier, 75004 Paris, France",
+  description: "",
+  storyline: storyline_bonus
+)
+file = URI.open("https://slack-imgs.com/?c=1&o1=ro&url=http%3A%2F%2Fwww.edarchitectes.net%2Fsite%2Fpeguy%2Fed_lycee_peguy_2.jpg")
+place102.photo.attach(io: file, filename: "lycee", content_type: "image/jpg")
+place102.save!
+
+# 103
+place103 = Place.new(
+  name: "Metro Parmentier",
+  address: "86 avenue Parmentier, 75004 Paris, France",
+  description: "Cette bouche de metro... que dire dessus sinon que je ne l'ai jamais prise!",
+  storyline: storyline_bonus
+)
+file = URI.open("https://img.cuisineaz.com/660x660/2013/12/20/i28721-recette-de-hachis-parmentier.webp")
+place103.photo.attach(io: file, filename: "metro_parmentier", content_type: "image/jpg")
+place103.save!
+
+# 104
+place104 = Place.new(
+  name: "104",
+  address: "99 avenue Jean Jaurès, 75019 Paris",
+  description: "",
+  storyline: storyline_bonus
+)
+file = URI.open("https://static.nationalgeographic.fr/files/styles/image_3200/public/01-auguste-louis-lumiere.jpg?w=710&h=788")
+place104.photo.attach(io: file, filename: "Frères-Lumière", content_type: "image/jpg")
+place104.save!
+
+
+riddle101 = Riddle.new(
+  title: 'Enigme 1',
+  description: 'Une première enigme pour se chauffer,',
+  question: "Quel est le nom du chat de Diane?",
+  solution: "swan",
+  motion_type: 'static',
+  place: place101
+)
+riddle101.save!
+
+riddle102 = Riddle.new(
+  title: 'Enigme 2',
+  description: "Maintenant, il est temps de sortir du Wagon et de rejoindre le bar.Enigme pour aller au deuxième lieu:\n
+  Le lieu se situe avenue parmentier.\n
+  Le numéro de l’avenue correspond à 4 fois l’âge d’une chanson célèbre de Pierre Bachelet.\n
+
+  Il s’agit d’un lieu d’apprentissage, mais ce n’est pas le wagon. Il faut un code pour y entrer, mais ce n’est pas en ruby. If age > 18 tu as peu de chance d’y aller encore, à moins d’en avoir fait son métier.
+  Son nom est celui d’un écrivain français du début du XXème siècle, connu pour ses poèmes et ses cahiers, mort au front en 1914.",
+  question: "Va à l’adresse et valide ta position!",
+  motion_type: 'shifting',
+  solution: "salut",
+  place: place101
+)
+riddle102.save!
+
+
+riddle103 = Riddle.new(
+  title: 'Enigme 3',
+  description: "Félicitations!Après le Lycée Charles Peguy il faut maintenant rejoindre une station de métro depuis ta position.\nPetite devinette:
+  \nMon premier est un adverbe
+  \nMon deuxième ne dit pas la vérité
+  \nMon troisième est l’appellation d’un prof assistant du wagon.",
+  question: "Quel est le nom de la station de métro?",
+  solution: "parmentier",
+  motion_type: 'shifting',
+  place: place102
+)
+riddle103.save!
+
+riddle104 = Riddle.new(
+  title: 'Enigme 4',
+  description: "Yes la bonne réponse était Parmentier ( par - 'ment' - 'T.A.' )\n
+  Du nom de ce bon vieux Antoine qui a importé la patate en France au XVIIIeme siècle et qui a laissé son nom au fameux hachis.\n
+  Dirige-toi vers la station de métro Parmentier et valide ta position devant l’entrée!",
+  question: "Valide ta position",
+  motion_type: 'shifting',
+  solution: "salut",
+  place: place103
+)
+riddle104.save!
+
+
+
+riddle105 = Riddle.new(
+  title: 'Enigme 5',
+  description: "Ok bien joué!
+  Pour éviter les problèmes de connexion dans le métro on vous donne la suite du trajet:
+  Prendre la 3 jusqu’à Répu puis la 5 direction Bobigny\n
+  Il ne reste plus qu’à trouver la bonne sortie:Enigme de dèv:\n
+  riddle = “Quel est le nom de famille des frères qui ont inventé le cinéma”\n
+  answer = ????\n
+  answer = answer.chars\n
+  subway_exit = answer.insert(1, 'A').join",
+  question: "subway_exit = ??? (l'écrire sans accent)",
+  motion_type: 'static',
+  solution: "laumiere",
+  place: place104
+)
+riddle105.save!
+
+
+
+riddle106 = Riddle.new(
+  title: 'Enigme 6',
+  description: "La prochaine enigme à trouver est un peu plus chaude, elle te donne l’adresse vers laquelle aller en sortant du métro.
+  D’abord le nom de la rue/avenue/boulevard : Je suis un homme d'État français, Mon nom résonne comme un cri de paix. Engagé pour les droits des travailleurs, Mon combat était empreint de ferveur.
+  J'ai défendu la justice sociale, Contre les inégalités, je m'étale. Assassiné pour mes idées ardentes, Mon nom reste synonyme de résistance.’
+  (écrire seulement le prénom et nom avec accents) ",
+  question: "Qui suis-je?",
+  solution: "jean jaurès",
+  motion_type: 'static',
+  place: place104
+)
+riddle106.save!
+
+riddle107 = Riddle.new(
+  title: 'Enigme 7',
+  description: "Oui c’était bien Jean Jaurès, assassiné en 1914 parce qu’il défendait une certaine idée de la France et militait pour la paix. Maintenant pour le numéro…\n
+  Petit bout de code à résoudre (dédicace à Romain):\n
+  family = Family.new\n
+  father = Dog.new(“Pongo”, family: family)\n
+  mother = Dog.new(“Perdita”, family: family)\n
+  street_number = family.kids.count\n
+  street_number = ???",
+  question: "Quel est le numéro?",
+  solution: "99",
+  motion_type: 'static',
+  place: place104
+)
+riddle107.save!
+
+
+riddle108 = Riddle.new(
+  title: 'Enigme 8',
+  description: "Nice! C'était la dernière énigme! l’adresse à valider est :
+  99 avenue Jean Jaurès
+
+  Pour la suite, l’adresse du bar étant  dans l’invit slack ça n'avait pas trop d’intérêt de la faire deviner, donc on se retrouve au bar Belushi's Paris Canal, 159 rue de Crimée, 75019 Paris
+
+  des bécots de la team CityHunters,
+  AP, Alexis, Henri et Pif",
+  motion_type: 'shifting',
+  solution: "salut",
+  place: place104
+)
+
+# file = URI.open('https://fondationolo.ca/wp-content/uploads/2017/07/fondation-olo-bebe-a-faim.jpg')
+# riddle108.photo.attach(io: file, filename: "eat_together", content_type: "image/jpg")
+riddle108.save!
+
+
+
+
+
 p "created #{Game.all.count} games"
 p "created #{Participation.all.count} participations"
 p "created #{User.all.count} users"
@@ -552,3 +748,5 @@ p "created #{Place.all.count} places"
 p "created #{Riddle.all.count} riddles"
 p "created #{Answer.all.count} answers"
 p "created #{Clue.all.count} clues"
+
+p storyline_bonus.title
