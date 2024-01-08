e623ca9c89418fc21282969ae67635772b434290
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2024-01-08 22:24:06 +0100

    Ajouter mini_racer pour la compilation des assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module CityHunters
       generate.helper false
       generate.test_framework :test_unit, fixture: false
       config.exceptions_app = self.routes
+      config.assets.js_compressor = :mini_racer
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
