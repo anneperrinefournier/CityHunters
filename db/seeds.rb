89730f905320be472281b2897ab58a7839a9dc70
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2024-01-28 16:53:20 +0100

    orthographe correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ require_relative 'users/dev_users'
 # Importing storylines
 require_relative 'storylines/opera_en_pagaille'
 require_relative 'storylines/code_rouge'
-# require_relative 'storylines/ruby_cest_fini'
-# require_relative 'storylines/food_call'
+require_relative 'storylines/ruby_cest_fini'
+require_relative 'storylines/food_call'
 # require_relative 'storylines/test_stories'
 
 # Checking that all places have address
