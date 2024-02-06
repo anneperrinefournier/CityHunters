78bd5ae82cd77a160a8982d150b5effa54d9a29d
AnnePerrine Fournier <anneperrine.fournier@gmail.com>
2024-02-06 04:15:26 +0100

    cleaning images

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,13 +1,15 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "QR code generation should update qr_code field" do
-    game = Game.create! # création d'une partie, ce qui va générer un QR code
-    game.generate_qr_code # méthode qui génère le QR code
+    monica = User.find_by(email: 'monica@gmail.com') # Récupération de l'utilisateur Monica de la DB
+    assert_not_nil monica, "User Monica not found in the database"
+
+    storyline = FactoryBot.create(:storyline, title: "Code Rouge")
+
+    game = Game.create!(user: monica, storyline: storyline) # Création d'une partie, ce qui va générer un QR code
+    game.generate_qr_code # Générer le QR code
+
     assert_not_nil game.qr_code, "QR code generation failed: qr_code field not updated"
     # Assurez-vous que le champ qr_code a été mis à jour avec une valeur non nulle
   end
