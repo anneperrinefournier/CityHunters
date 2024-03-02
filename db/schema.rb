686566c1d3a222707ab25f6e1e5a0bc8a37114ca
Alexis De_Temmerman <de.temmerman.alexis@gmail.com>
2024-03-02 17:44:30 +0100

    Update state message db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_210608) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_130418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_210608) do
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "index"
+    t.string "data_type"
     t.index ["game_id"], name: "index_state_messages_on_game_id"
   end
 
